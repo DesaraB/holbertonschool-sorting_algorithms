d9a20bd84f4752373193389ee4ab1e126c296385
“DesaraB” <desarabaroti99@gmail.com>
2024-12-12 10:53:49 +0000

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,9 +12,11 @@ void bubble_sort(int *array, size_t size)
 	size_t i;
 	size_t j;
 	int tmp;
+	int swapped;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = 0;
 		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -22,9 +24,12 @@ void bubble_sort(int *array, size_t size)
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
+				swapped = 1;
 
-				print_array(array, size);
 			}
 		}
+		if (swapped == 0)
+			break;
+		print_array(array, size);
 	}
 }
